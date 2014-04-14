40edff34dcd9774ca43a2ecb0c2057cfd2d02143
lmene024 <lmene024@fiu.edu>
2014-04-13 21:29:29 -0400

    added shopping form

---
+++ VBExcerciseClub/VBExcerClub/frmShopping.Designer.vb
@@ -0,0 +1,382 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmShopping
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.tsbHome = New System.Windows.Forms.ToolStripButton()
+        Me.tsbMember = New System.Windows.Forms.ToolStripButton()
+        Me.tsbProgram = New System.Windows.Forms.ToolStripButton()
+        Me.tsbShop = New System.Windows.Forms.ToolStripButton()
+        Me.tsbContact = New System.Windows.Forms.ToolStripButton()
+        Me.tsbExit = New System.Windows.Forms.ToolStripButton()
+        Me.tsbHelp = New System.Windows.Forms.ToolStripButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblOrderNum = New System.Windows.Forms.Label()
+        Me.lsvLines = New System.Windows.Forms.ListView()
+        Me.lblMemName = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnConfirmOrder = New System.Windows.Forms.Button()
+        Me.btnCancelOrder = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.grpItems = New System.Windows.Forms.GroupBox()
+        Me.lstItems = New System.Windows.Forms.ListBox()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.cboMembers = New System.Windows.Forms.ComboBox()
+        Me.btnNew = New System.Windows.Forms.Button()
+        Me.ToolStrip1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.grpItems.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.AutoSize = False
+        Me.ToolStrip1.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsbHome, Me.tsbMember, Me.tsbProgram, Me.tsbShop, Me.tsbContact, Me.tsbExit, Me.tsbHelp})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(820, 75)
+        Me.ToolStrip1.TabIndex = 1
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'tsbHome
+        '
+        Me.tsbHome.AutoSize = False
+        Me.tsbHome.BackgroundImage = Global.VBExcerClub.My.Resources.Resources.home_2
+        Me.tsbHome.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.tsbHome.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsbHome.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbHome.Name = "tsbHome"
+        Me.tsbHome.Size = New System.Drawing.Size(65, 70)
+        Me.tsbHome.Text = "Home"
+        '
+        'tsbMember
+        '
+        Me.tsbMember.AutoSize = False
+        Me.tsbMember.BackgroundImage = Global.VBExcerClub.My.Resources.Resources.person_1
+        Me.tsbMember.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.tsbMember.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsbMember.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbMember.Name = "tsbMember"
+        Me.tsbMember.Size = New System.Drawing.Size(65, 70)
+        Me.tsbMember.Text = "Member"
+        '
+        'tsbProgram
+        '
+        Me.tsbProgram.AutoSize = False
+        Me.tsbProgram.BackgroundImage = Global.VBExcerClub.My.Resources.Resources.barbells
+        Me.tsbProgram.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.tsbProgram.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsbProgram.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbProgram.Name = "tsbProgram"
+        Me.tsbProgram.Size = New System.Drawing.Size(65, 70)
+        Me.tsbProgram.Text = "Program"
+        '
+        'tsbShop
+        '
+        Me.tsbShop.AutoSize = False
+        Me.tsbShop.BackgroundImage = Global.VBExcerClub.My.Resources.Resources.shopping
+        Me.tsbShop.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.tsbShop.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsbShop.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbShop.Name = "tsbShop"
+        Me.tsbShop.Size = New System.Drawing.Size(65, 70)
+        Me.tsbShop.Text = "Shopping"
+        '
+        'tsbContact
+        '
+        Me.tsbContact.AutoSize = False
+        Me.tsbContact.BackgroundImage = Global.VBExcerClub.My.Resources.Resources.email
+        Me.tsbContact.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.tsbContact.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsbContact.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbContact.Name = "tsbContact"
+        Me.tsbContact.Size = New System.Drawing.Size(65, 70)
+        Me.tsbContact.Text = "Shopping"
+        '
+        'tsbExit
+        '
+        Me.tsbExit.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.tsbExit.AutoSize = False
+        Me.tsbExit.BackgroundImage = Global.VBExcerClub.My.Resources.Resources.exit3
+        Me.tsbExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.tsbExit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsbExit.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbExit.Name = "tsbExit"
+        Me.tsbExit.Size = New System.Drawing.Size(65, 70)
+        Me.tsbExit.Text = "Exit"
+        '
+        'tsbHelp
+        '
+        Me.tsbHelp.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.tsbHelp.AutoSize = False
+        Me.tsbHelp.BackgroundImage = Global.VBExcerClub.My.Resources.Resources.question
+        Me.tsbHelp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.tsbHelp.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsbHelp.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsbHelp.Name = "tsbHelp"
+        Me.tsbHelp.Size = New System.Drawing.Size(65, 70)
+        Me.tsbHelp.Text = "Help"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.lblOrderNum)
+        Me.GroupBox1.Controls.Add(Me.lsvLines)
+        Me.GroupBox1.Controls.Add(Me.lblMemName)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(544, 82)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(264, 367)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        '
+        'Label4
+        '
+        Me.Label4.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(129, 326)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(129, 26)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Total"
+        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label3
+        '
+        Me.Label3.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(129, 300)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(129, 26)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Tax"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(129, 274)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(129, 26)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "SubTotal"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'lblOrderNum
+        '
+        Me.lblOrderNum.BackColor = System.Drawing.Color.White
+        Me.lblOrderNum.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblOrderNum.Location = New System.Drawing.Point(7, 55)
+        Me.lblOrderNum.Name = "lblOrderNum"
+        Me.lblOrderNum.Size = New System.Drawing.Size(70, 18)
+        Me.lblOrderNum.TabIndex = 3
+        '
+        'lsvLines
+        '
+        Me.lsvLines.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lsvLines.Location = New System.Drawing.Point(7, 76)
+        Me.lsvLines.Name = "lsvLines"
+        Me.lsvLines.Size = New System.Drawing.Size(251, 190)
+        Me.lsvLines.TabIndex = 2
+        Me.lsvLines.UseCompatibleStateImageBehavior = False
+        '
+        'lblMemName
+        '
+        Me.lblMemName.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lblMemName.AutoEllipsis = True
+        Me.lblMemName.BackColor = System.Drawing.Color.White
+        Me.lblMemName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblMemName.Location = New System.Drawing.Point(83, 55)
+        Me.lblMemName.Name = "lblMemName"
+        Me.lblMemName.Size = New System.Drawing.Size(175, 18)
+        Me.lblMemName.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.BackColor = System.Drawing.Color.DarkViolet
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(7, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(251, 39)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Order"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnConfirmOrder
+        '
+        Me.btnConfirmOrder.Location = New System.Drawing.Point(549, 463)
+        Me.btnConfirmOrder.Name = "btnConfirmOrder"
+        Me.btnConfirmOrder.Size = New System.Drawing.Size(75, 23)
+        Me.btnConfirmOrder.TabIndex = 3
+        Me.btnConfirmOrder.Text = "Confirm"
+        Me.btnConfirmOrder.UseVisualStyleBackColor = True
+        '
+        'btnCancelOrder
+        '
+        Me.btnCancelOrder.Location = New System.Drawing.Point(727, 463)
+        Me.btnCancelOrder.Name = "btnCancelOrder"
+        Me.btnCancelOrder.Size = New System.Drawing.Size(75, 23)
+        Me.btnCancelOrder.TabIndex = 4
+        Me.btnCancelOrder.Text = "Cancel"
+        Me.btnCancelOrder.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.btnNew)
+        Me.GroupBox2.Controls.Add(Me.cboMembers)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 82)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(516, 77)
+        Me.GroupBox2.TabIndex = 5
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Member"
+        '
+        'grpItems
+        '
+        Me.grpItems.Controls.Add(Me.btnAdd)
+        Me.grpItems.Controls.Add(Me.btnSearch)
+        Me.grpItems.Controls.Add(Me.txtSearch)
+        Me.grpItems.Controls.Add(Me.lstItems)
+        Me.grpItems.Location = New System.Drawing.Point(12, 171)
+        Me.grpItems.Name = "grpItems"
+        Me.grpItems.Size = New System.Drawing.Size(515, 314)
+        Me.grpItems.TabIndex = 6
+        Me.grpItems.TabStop = False
+        Me.grpItems.Text = "Items"
+        '
+        'lstItems
+        '
+        Me.lstItems.FormattingEnabled = True
+        Me.lstItems.Location = New System.Drawing.Point(10, 53)
+        Me.lstItems.Name = "lstItems"
+        Me.lstItems.Size = New System.Drawing.Size(493, 212)
+        Me.lstItems.TabIndex = 0
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Location = New System.Drawing.Point(10, 19)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(411, 20)
+        Me.txtSearch.TabIndex = 1
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Location = New System.Drawing.Point(428, 19)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(75, 23)
+        Me.btnSearch.TabIndex = 2
+        Me.btnSearch.Text = "Search"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(428, 276)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 3
+        Me.btnAdd.Text = "Add to Order"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'cboMembers
+        '
+        Me.cboMembers.FormattingEnabled = True
+        Me.cboMembers.Location = New System.Drawing.Point(10, 27)
+        Me.cboMembers.Name = "cboMembers"
+        Me.cboMembers.Size = New System.Drawing.Size(369, 21)
+        Me.cboMembers.TabIndex = 0
+        '
+        'btnNew
+        '
+        Me.btnNew.Location = New System.Drawing.Point(428, 25)
+        Me.btnNew.Name = "btnNew"
+        Me.btnNew.Size = New System.Drawing.Size(75, 23)
+        Me.btnNew.TabIndex = 1
+        Me.btnNew.Text = "New Order"
+        Me.btnNew.UseVisualStyleBackColor = True
+        '
+        'frmShopping
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(179, Byte), Integer), CType(CType(236, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(820, 492)
+        Me.Controls.Add(Me.grpItems)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.btnCancelOrder)
+        Me.Controls.Add(Me.btnConfirmOrder)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.ForeColor = System.Drawing.Color.DarkBlue
+        Me.Name = "frmShopping"
+        Me.Text = "frmShopping"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.grpItems.ResumeLayout(False)
+        Me.grpItems.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ToolStrip1 As System.Windows.Forms.ToolStrip
+    Friend WithEvents tsbHome As System.Windows.Forms.ToolStripButton
+    Friend WithEvents tsbMember As System.Windows.Forms.ToolStripButton
+    Friend WithEvents tsbProgram As System.Windows.Forms.ToolStripButton
+    Friend WithEvents tsbShop As System.Windows.Forms.ToolStripButton
+    Friend WithEvents tsbContact As System.Windows.Forms.ToolStripButton
+    Friend WithEvents tsbExit As System.Windows.Forms.ToolStripButton
+    Friend WithEvents tsbHelp As System.Windows.Forms.ToolStripButton
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents lblOrderNum As System.Windows.Forms.Label
+    Friend WithEvents lsvLines As System.Windows.Forms.ListView
+    Friend WithEvents lblMemName As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnConfirmOrder As System.Windows.Forms.Button
+    Friend WithEvents btnCancelOrder As System.Windows.Forms.Button
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents btnNew As System.Windows.Forms.Button
+    Friend WithEvents cboMembers As System.Windows.Forms.ComboBox
+    Friend WithEvents grpItems As System.Windows.Forms.GroupBox
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents btnSearch As System.Windows.Forms.Button
+    Friend WithEvents txtSearch As System.Windows.Forms.TextBox
+    Friend WithEvents lstItems As System.Windows.Forms.ListBox
+End Class
---
+++ VBExcerciseClub/VBExcerClub/frmShopping.vb
@@ -0,0 +1,4 @@
+﻿Public Class frmShopping
+
+  
+End Class
