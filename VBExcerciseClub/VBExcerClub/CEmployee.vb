898012bdc6172bd49d2a279bd3fa700205865d19
lmene024 <lmene024@fiu.edu>
2014-04-19 15:32:35 -0400

    Added Employee Classes

---
+++ VBExcerciseClub/VBExcerClub/CEmployee.vb
@@ -0,0 +1,160 @@
+﻿Public Class CEmployee
+    'represents a single record in the Employee table
+    'declare the private instance variables
+    Private _mstrEmpID As String
+    Private _mstrLName As String
+    Private _mstrFName As String
+    Private _mstrAddress As String
+    Private _mstrCity As String
+    Private _mstrState As String
+    Private _mstrZip As String
+    Private _mstrPhone As String
+    Private _mdtmDateHired As DateTime
+    Private _msglCommission As Single
+    Private _mstrRoleID As String
+    Private _isNewEmployee As Boolean
+    Private _hasChanges As Boolean
+    'constructor
+    Public Sub New()
+        'initialize class instance variables
+        _mstrEmpID = ""
+        'do the rest here
+        'date values are initialized to 1/1/0001 by default, so we set it to today instead
+        _mdtmDateHired = New Date(Now.Year, Now.Month, Now.Day)
+    End Sub
+    'now do the properties (getters & setters)
+    Public Property EmpID() As String
+        Get
+            Return _mstrEmpID
+        End Get
+        Set(strEmpID As String)
+            _mstrEmpID = strEmpID
+        End Set
+    End Property
+    Public Property LName() As String
+        Get
+            Return _mstrLName
+        End Get
+        Set(strLName As String)
+            _mstrLName = strLName
+        End Set
+    End Property
+    Public Property FName() As String
+        Get
+            Return _mstrFName
+        End Get
+        Set(strFName As String)
+            _mstrFName = strFName
+        End Set
+    End Property
+    Public Property Address() As String
+        Get
+            Return _mstrAddress
+        End Get
+        Set(strAddress As String)
+            _mstrAddress = strAddress
+        End Set
+    End Property
+    Public Property City() As String
+        Get
+            Return _mstrCity
+        End Get
+        Set(strCity As String)
+            _mstrCity = strCity
+        End Set
+    End Property
+    Public Property State() As String
+        Get
+            Return _mstrState
+        End Get
+        Set(strState As String)
+            _mstrState = strState
+        End Set
+    End Property
+    Public Property Zip() As String
+        Get
+            Return _mstrZip
+        End Get
+        Set(strZip As String)
+            _mstrZip = strZip
+        End Set
+    End Property
+    Public Property Phone() As String
+        Get
+            Return _mstrPhone
+        End Get
+        Set(strPhone As String)
+            _mstrPhone = strPhone
+        End Set
+    End Property
+    Public Property DateHired() As DateTime
+        Get
+            Return _mdtmDateHired
+        End Get
+        Set(_mdtmateHired As DateTime)
+            _mdtmDateHired = _mdtmateHired
+        End Set
+    End Property
+    Public Property RoleID As String
+        Get
+            Return _mstrRoleID
+        End Get
+        Set(strRoleID As String)
+            _mstrRoleID = strRoleID
+        End Set
+    End Property
+    Public Property hasChanges() As Boolean
+        Get
+            Return _hasChanges
+        End Get
+        Set(value As Boolean)
+            _hasChanges = value
+        End Set
+    End Property
+    Public Property Commission() As Single
+        Get
+            Return _msglCommission
+        End Get
+        Set(sglCommission As Single)
+            _msglCommission = sglCommission
+        End Set
+    End Property
+    Public Property IsNewEmployee() As Boolean
+        Get
+            Return _isNewEmployee
+        End Get
+        Set(value As Boolean)
+            _isNewEmployee = value
+        End Set
+    End Property
+
+    Public ReadOnly Property GetSaveParameters() As ArrayList
+        Get
+            Dim paramList As New ArrayList()
+            paramList.Add(New SqlClient.SqlParameter("empid", _mstrEmpID))
+            paramList.Add(New SqlClient.SqlParameter("lname", _mstrLName))
+            paramList.Add(New SqlClient.SqlParameter("fname", _mstrFName))
+            paramList.Add(New SqlClient.SqlParameter("address", _mstrAddress))
+            paramList.Add(New SqlClient.SqlParameter("city", _mstrCity))
+            paramList.Add(New SqlClient.SqlParameter("state", _mstrState))
+            paramList.Add(New SqlClient.SqlParameter("zip", _mstrZip))
+            paramList.Add(New SqlClient.SqlParameter("phone", _mstrPhone))
+            paramList.Add(New SqlClient.SqlParameter("datehired", _mdtmDateHired))
+            paramList.Add(New SqlClient.SqlParameter("commissionrate", _msglCommission))
+            paramList.Add(New SqlClient.SqlParameter("roleid", _mstrRoleID))
+            Return paramList
+        End Get
+    End Property
+
+    Public Function Save() As Integer
+        'return -1 if the ID already exists (and we can't create a new record then
+        If IsNewEmployee Then
+            Dim strRes As String = myDB.GetSingleValueFromSP("sp_CheckEmployeeIDExists", New SqlClient.SqlParameter("EMPID", _mstrEmpID))
+            If Not strRes = 0 Then
+                Return -1 'Not UNIQUE!
+            End If
+        End If
+        'if not a new member or is new and is unique ID then do the save (update or insert)
+        Return myDB.ExecSP("sp_SaveEmployee", GetSaveParameters())
+    End Function
+End Class
---
+++ VBExcerciseClub/VBExcerClub/CEmployees.vb
@@ -0,0 +1,96 @@
+﻿Imports System.Data.SqlClient
+Public Class CEmployees
+    'This class represents the Employeed table and the associated business rules for it
+    Private _Employee As CEmployee
+
+    'constructor
+    Public Sub New()
+        'instantiate the CMember object
+        _Employee = New CEmployee
+    End Sub
+
+    Public ReadOnly Property CurrentObject() As CEmployee
+        Get
+            Return _Employee
+        End Get
+    End Property
+
+    Public Function IsValidHiredDate(dtmDate As Date) As Boolean
+        'ensure that the date is today or earlier, and not a future date
+        If dtmDate <= Today Then 'this is not a future date
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Public Sub Clear()
+        _Employee = New CEmployee() 'so much faster this way
+    End Sub
+
+    Public Sub CreateNewMember() 'Call me when you're clearing the screen to add a new student!
+        'This clears the old student and sets it as new
+        Clear()
+        _Employee.IsNewEmployee = True
+    End Sub
+
+    Public Function Save() As Integer
+        Return _Employee.Save()
+    End Function
+
+    Public Function GetEmployeeList() As SqlDataReader
+        Return myDB.GetDataReaderBySP("dbo.sp_GetEmployeeList")
+    End Function
+
+    Public Function GetEmployeePhoneList() As SqlDataReader
+        Return myDB.GetDataReaderBySP("dbo.sp_GetEmployeePhoneList")
+    End Function
+
+    Public Function GetEmployeeByName(strLname As String, strFname As String) As CEmployee
+        '  Dim params(0) As SqlParameter
+        Dim params As New ArrayList
+        Dim param1 As New SqlParameter("lname", strLname)
+        params.Add(param1)
+        Dim param2 As New SqlParameter("fname", strFname)
+        params.Add(param2)
+        FillObject(myDB.GetDataReaderBySP("dbo.sp_GetEmployeeByName", params))
+        Return _Employee
+    End Function
+
+    Public Function GetEmployeeByPhone(strPhone As String) As CEmployee
+        Dim aParam As New SqlParameter("phone", strPhone)
+        FillObject(myDB.GetDataReaderBySP("dbo.sp_GetEmployeeByPhone", aParam))
+        Return _Employee
+    End Function
+
+    Public Function GetMemberByID(strID As String) As CEmployee
+        Dim aParam As New SqlParameter("ID", strID)
+        FillObject(myDB.GetDataReaderBySP("dbo.sp_GetEmployeeByID", aParam))
+        Return _Employee
+    End Function
+
+    Public Function FillObject(sqlDR As SqlDataReader) As CEmployee
+        'why persist the object, just pass updateObject with the datareader
+        'you could also isnull your select statement fields to get rid of all that appending
+        Using sqlDR
+            If sqlDR.Read() Then
+                With _Employee
+                    .EmpID = sqlDR.Item("EmpID") & ""
+                    .LName = sqlDR.Item("LName") & ""
+                    .FName = sqlDR.Item("FName") & ""
+                    .Address = sqlDR.Item("Address") & ""
+                    .City = sqlDR.Item("City") & ""
+                    .State = sqlDR.Item("State") & ""
+                    .Zip = sqlDR.Item("Zip") & ""
+                    .Phone = sqlDR.Item("Phone") & ""
+                    .DateHired = SafeDate(sqlDR.Item("DateHired").ToString())  '& ""
+                    .Commission = sqlDR.Item("CommissionRate") & ""
+                    .RoleID = sqlDR.Item("RoleID") & ""
+                End With
+            Else
+                'failed
+            End If
+        End Using
+        Return _Employee
+    End Function
+End Class
