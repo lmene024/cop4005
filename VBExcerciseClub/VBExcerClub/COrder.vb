11f5d93dc0ff5af6bc6cd664c5494f3e1f6f3b39
tyranx <tyranxzero@gmail.com>
2014-04-21 21:23:45 -0400

    added code to COrder class

--- VBExcerciseClub/VBExcerClub/COrder.vb
+++ VBExcerciseClub/VBExcerClub/COrder.vb
@@ -1,3 +1,72 @@
 ﻿Public Class COrder
+    Private _mstrInvoiceID As String
+    Private _mstrMbrID As String
+    Private _mstrEmpID As String
+    Private _msglInvTotal As Single
+    Private _msglProdTotal As Single
+    Private _msglTaxTotal As Single
+    Private _mdtmInvoiceDate As DateTime
+    Private _marrItems As ArrayList
 
+    'constructor
+    Public Sub New()
+        _mstrInvoiceID = ""
+        _mstrEmpID = ""
+        _mstrMbrID = ""
+        _mdtmInvoiceDate = New Date(Now.Year, Now.Month, Now.Day)
+    End Sub
+
+    Public Property InvoiceID As String
+        Get
+            Return _mstrInvoiceID
+        End Get
+        Set(strInvoiceID As String)
+            _mstrInvoiceID = strInvoiceID
+        End Set
+    End Property
+
+    Public Property MbrID As String
+        Get
+            Return _mstrMbrID
+        End Get
+        Set(strMbrID As String)
+            _mstrMbrID = strMbrID
+        End Set
+    End Property
+
+    Public Property EmpID As String
+        Get
+            Return _mstrEmpID
+        End Get
+        Set(strEmpID As String)
+            _mstrEmpID = strEmpID
+        End Set
+    End Property
+
+    Public Property InvTotal As Single
+        Get
+            Return _msglInvTotal
+        End Get
+        Set(sglInvTotal As Single)
+            _msglInvTotal = sglInvTotal
+        End Set
+    End Property
+
+    Public Property ProdTotal As Single
+        Get
+            Return _msglProdTotal
+        End Get
+        Set(sglProdTotal As Single)
+            _msglProdTotal = sglProdTotal
+        End Set
+    End Property
+
+    Public Property TaxTotal As Single
+        Get
+            Return _msglTaxTotal
+        End Get
+        Set(sglTaxTotal As Single)
+            _msglTaxTotal = sglTaxTotal
+        End Set
+    End Property
 End Class
