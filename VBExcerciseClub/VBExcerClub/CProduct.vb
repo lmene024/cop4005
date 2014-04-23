b20a150c5f4386daf86a0d64c30608d848adbded
aforg001 <aforg001@fiu.edu>
2014-04-23 18:24:15 -0400

    Added CProduct & CProducts

---
+++ VBExcerciseClub/VBExcerClub/CProduct.vb
@@ -0,0 +1,115 @@
+﻿Public Class CProduct
+    Private _mstrProdID As String
+    Private _mstrProdDesc As String
+    Private _msglWhCost As String
+    Private _msglRetPrice As String
+    Private _isTaxable As Boolean
+    Private _isNewProduct As Boolean
+    Private _hasChanges As Boolean
+    'constructor
+    Public Sub New()
+        'SetProductID()
+        _mstrProdID = ""
+    End Sub
+
+    'Public Sub SetProductID()
+    '    Dim strLastInvoice As String = myDB.GetSingleValueFromSP("sp_GetLastProductID")
+    '    Dim strInvoiceID As String
+
+    '    If String.IsNullOrEmpty(strLastInvoice) Then
+    '        strInvoiceID = "I0001"
+    '    Else
+    '        strInvoiceID = "I" + ((CType((Right(strLastInvoice, 4)), Integer)) + 1)
+    '        _mstrProdID = strInvoiceID
+    '    End If
+
+    'End Sub
+
+    Public Property ProductID As String
+        Get
+            Return _mstrProdID
+        End Get
+        Set(strProdID As String)
+            _mstrProdID = strProdID
+        End Set
+    End Property
+
+    Public Property ProductDescription As String
+        Get
+            Return _mstrProdDesc
+        End Get
+        Set(strProdDesc As String)
+            _mstrProdDesc = strProdDesc
+        End Set
+    End Property
+
+    Public Property WhCost As String
+        Get
+            Return _msglWhCost
+        End Get
+        Set(strWhCost As String)
+            _msglWhCost = strWhCost
+        End Set
+    End Property
+
+    Public Property RetailPrice As Single
+        Get
+            Return _msglRetPrice
+        End Get
+        Set(strRetailPrice As Single)
+            _msglRetPrice = strRetailPrice
+        End Set
+    End Property
+
+    Private Property isTaxable As Boolean
+        Get
+            Return _isTaxable
+        End Get
+        Set(isTaxable As Boolean)
+            _isTaxable = isTaxable
+        End Set
+    End Property
+
+    Public Property hasChanges() As Boolean
+        Get
+            Return _hasChanges
+        End Get
+        Set(hasChanges As Boolean)
+            _hasChanges = hasChanges
+        End Set
+    End Property
+
+    Public Property IsNewProduct() As Boolean
+        Get
+            Return _isNewProduct
+        End Get
+        Set(isNewProduct As Boolean)
+            _isNewProduct = isNewProduct
+        End Set
+    End Property
+
+    Public ReadOnly Property GetSaveParameters() As ArrayList
+        Get
+            Dim paramList As New ArrayList()
+            paramList.Add(New SqlClient.SqlParameter("prodid", _mstrProdID))
+            paramList.Add(New SqlClient.SqlParameter("proddesc", _mstrProdDesc))
+            paramList.Add(New SqlClient.SqlParameter("whcost", _msglWhCost))
+            paramList.Add(New SqlClient.SqlParameter("retprice", _msglRetPrice))
+            paramList.Add(New SqlClient.SqlParameter("istaxable", _isTaxable))
+
+            Return paramList
+        End Get
+    End Property
+
+    Public Function Save() As Integer
+        'return -1 if the ID already exists (and we can't create a new record then
+        If IsNewProduct Then
+            Dim strRes As String = myDB.GetSingleValueFromSP("sp_CheckProductIDExists", New SqlClient.SqlParameter("PRODID", _mstrProdID))
+            If Not strRes = 0 Then
+                Return -1 'Not UNIQUE!
+            End If
+        End If
+        'if not a new member or is new and is unique ID then do the save (update or insert)
+        Return myDB.ExecSP("sp_SaveOrder", GetSaveParameters())
+    End Function
+End Class
---
+++ VBExcerciseClub/VBExcerClub/CProducts.vb
@@ -0,0 +1,75 @@
+﻿Imports System.Data.SqlClient
+Public Class CProducts
+    'This class represents the ProdInvoice table and the associated business rules for it
+    Private _Product As CProduct
+
+    'constructor
+    Public Sub New()
+        'instantiate the COrder object
+        _Product = New CProduct
+    End Sub
+
+    'Public ReadOnly Property CurrentObject() As CProduct
+    '    Get
+    '        Return _Product
+    'End Property
+
+    'Public Function IsValidInvoiceDate(dtmDate As Date) As Boolean
+    '    'ensure that the date is today or earlier, and not a future date
+    '    If dtmDate <= Today Then 'this is not a future date
+    '        Return True
+    '    Else
+    '        Return False
+    '    End If
+    'End Function
+
+    Public Sub Clear()
+        _Product = New CProduct() 'so much faster this way
+    End Sub
+
+    Public Sub CreateNewProduct() 'Call me when you're clearing the screen to add a new student!
+        'This clears the old student and sets it as new
+        Clear()
+        _Product.IsNewProduct = True
+    End Sub
+
+    Public Function Save() As Integer
+        Return _Product.Save()
+    End Function
+
+    Public Function GetProductList() As SqlDataReader
+        Return myDB.GetDataReaderBySP("dbo.sp_GetProductList")
+    End Function
+
+    Public Function GetProductByID(strID As String) As CProduct
+        Dim aParam As New SqlParameter("ID", strID)
+        FillObject(myDB.GetDataReaderBySP("dbo.sp_GetProductByID", aParam))
+        Return _Product
+    End Function
+
+    Public Function GetProductByDesc(strDesc As String) As CProduct
+        Dim aParam As New SqlParameter("Description", strDesc)
+        FillObject(myDB.GetDataReaderBySP("dbo.sp_GetProductByDesc", aParam))
+        Return _Product
+    End Function
+
+    Public Function FillObject(sqlDR As SqlDataReader) As CProduct
+        'why persist the object, just pass updateObject with the datareader
+        'you could also isnull your select statement fields to get rid of all that appending
+        Using sqlDR
+            If sqlDR.Read() Then
+                With _Product
+                    .ProductID = sqlDR.Item("ProductID") & ""
+                    .ProductDescription = sqlDR.Item("ProductDescription") & ""
+                    .WhCost = sqlDR.Item("WhCost") & ""
+                    .RetailPrice = sqlDR.Item("RetailPrice") & ""
+                    'Do not know if 'isTaxable' is needed here
+
+                End With
+            Else
+                'failed
+            End If
+        End Using
+        Return _Product
+    End Function
+End Class
